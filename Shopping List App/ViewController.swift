78dffbbf226d62542cbfb54865d5ed007294ade6
Alan <csuen2@my.centennialcollege.ca>
2022-10-27 01:33:08 -0400

    Create project

---
+++ Shopping List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping List App
+//
+//  Created by alan on 27/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
